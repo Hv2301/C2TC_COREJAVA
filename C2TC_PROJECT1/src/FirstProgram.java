48ae3dc59ba7364b9e4bc5172236a7ce2171945a
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-07 19:13:08 +0530

    first commit

---
+++ C2TC_PROJECT1/src/FirstProgram.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class FirstProgram {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
