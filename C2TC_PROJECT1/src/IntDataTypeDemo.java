ec5c75c80183d56a4df1724895e7e82ecd5388d6
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-10 22:37:40 +0530

    first commit

---
+++ C2TC_PROJECT1/src/IntDataTypeDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class IntDataTypeDemo {
+
+	public static void main(String[] args) {
+		int max = 2147483647;
+		int min = -2147483647;
+		short shortMax = 32767;
+		short shortMin = -32768;
+		long longMax  = 9223372036854775807L;
+		long longMin = -9223372036854775807L;
+		byte byteMax = 127;
+		byte byteMin = -128;
+	}
+
+}
