023f68869940b4f0ba743e07fffc6b1d4c506c40
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-11 18:09:43 +0530

    first commit

---
+++ C2TC_PROJECT1/src/ArithmeticOperatorsDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class ArithmeticOperatorsDemo {
+
+	
+	public static void main(String[] args) {
+		int x =10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+	}
+
+}
