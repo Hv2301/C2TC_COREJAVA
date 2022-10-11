b8fed3fb31c0d1e0b3dac7e992c41144e46946dd
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-11 18:14:48 +0530

    first commit

---
+++ C2TC_PROJECT1/src/AssignmentOperatorsDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class AssignmentOperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int x = 4;
+     x+=5;//x=x+5;
+     //x-=5;//x=x-5;
+     //x*=5;//x=x*5;
+     //x/=5;//x=x/5;
+     //x%=5;//x=x%5;
+     System.out.println(x);
+	}
+
+}
