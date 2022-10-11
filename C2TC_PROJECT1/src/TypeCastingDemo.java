104b67c733202c8f2c999747abede69f900c2a46
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-11 18:06:45 +0530

    first commit

---
+++ C2TC_PROJECT1/src/TypeCastingDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class TypeCastingDemo {
+
+	
+	public static void main(String[] args) {
+		float f = 10.532f;
+		long l = (long)f;
+		double d = f;
+		System.out.println(l);
+		byte b =12;
+		short s = b; //widening
+		System.out.println(b);
+		
+	}
+
+}
