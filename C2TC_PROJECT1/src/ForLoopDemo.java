6d7ab28ff0add1abf12cd515c2ac4c64ae004599
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-12 21:42:55 +0530

    first commit

---
+++ C2TC_PROJECT1/src/ForLoopDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class ForLoopDemo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    for(int i=1;i<=5;i++)//(int i=1000;i>=1;i--) {
+    	System.out.print("Value of i:"+i);
+       
+    }
+	}
+
+
