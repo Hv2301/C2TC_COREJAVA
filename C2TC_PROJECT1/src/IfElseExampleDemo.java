c3fe787037ee9291bf6df03e2649b04a9b3086e8
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-12 21:30:06 +0530

    first commit

---
+++ C2TC_PROJECT1/src/IfElseExampleDemo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class IfElseExampleDemo {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int ageofBoy = 36;
+     int ageofGirl = 25;
+     if((ageofBoy>=21)&&(ageofGirl>=18))
+     {
+    	 System.out.println("ready to get married!");
+     }
+     else {
+    	 System.out.println("Wait for it kiddo!");
+     }
+	}
+
+}
