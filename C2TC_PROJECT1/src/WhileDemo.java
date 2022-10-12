b9401113e3ca176dd73b7fdb8342f50bda3257c2
carol sanjana p <carol sanjana p@DESKTOP-CFAQCC3>
2022-10-12 21:48:16 +0530

    first commit

---
+++ C2TC_PROJECT1/src/WhileDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author carol sanjana p
+ *
+ */
+public class WhileDemo {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int i = 99;
+     while (i<=100) {
+    	 System.out.println(i);
+    	 i++;
+    	 
+     }
+	}
+
+}
